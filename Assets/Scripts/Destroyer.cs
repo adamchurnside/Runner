a5374829f2439462008af4e435e10a670c7e0e58
Adam Churnside <adam.churnside@gmail.com>
2020-03-24 16:02:20 -0700

    Added destroy objects.

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public float litetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, litetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
