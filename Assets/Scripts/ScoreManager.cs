f5711cb3f6db7ae72d243c20b1d8d92efd54d8ec
Adam Churnside <adam.churnside@gmail.com>
2020-03-24 17:14:01 -0700

    Added obstacle collision and score manager.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public int score;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            score++;
+            Debug.Log(score);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
