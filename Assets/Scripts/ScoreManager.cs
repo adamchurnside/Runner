5cc0ea93ed453d04de7a44c506a605157ee6f06f
Adam Churnside <adam.churnside@gmail.com>
2020-03-25 11:25:50 -0700

    Added score UI code.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score;
+    public Text scoreDisplay;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        scoreDisplay.text = score.ToString();
         if (collision.CompareTag("Obstacle"))
         {
             score++;
