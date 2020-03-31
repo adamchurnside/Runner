e72f5a329c0bebc693e8a3caa87de43b1f750ef3
Adam Churnside <adam.churnside@gmail.com>
2020-03-31 14:19:51 -0700

    Added options menu.

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void setVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void setQuality(int qualityIndex) 
+    {
+        Debug.Log(qualityIndex);
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    
+}
