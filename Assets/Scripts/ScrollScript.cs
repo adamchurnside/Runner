5199be746cbcd165def5545dd0765fbbb8e5677d
Adam Churnside <adam.churnside@gmail.com>
2020-04-03 20:09:15 -0700

    Added background scroll functionality.

---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0;
+    public Renderer backgroundRenderer;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundRenderer.material.mainTextureOffset = new Vector2(Time.time * speed, 0f);
+    }
+}
